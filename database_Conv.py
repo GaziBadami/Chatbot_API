530584e9d2df2d998d54c7ed2b5af32a97e2aec0
GaziBadami <badamigazi@gmail.com>
2026-02-09 18:38:38 +0530

    g

--- database_Conv.py
+++ database_Conv.py
@@ -18,7 +18,7 @@ def get_db_connection():
     try:
         return mysql.connector.connect(
             host=os.getenv("DB_HOST", "localhost"),
-            port=int(os.getenv("DB_PORT", "3306")),
+            port=24253,
             user=os.getenv("DB_USER", "root"),
             password=os.getenv("DB_PASSWORD", ""),
             database=os.getenv("DB_NAME", "chatbot_db"),
